e3e6609a45bf9684e4507e77e867d203e14fed1d
Maya Doshi <maya-doshi@proton.me>
2025-04-27 02:44:56 -0400

    switch out lastfm for musicbrainz (no api key needed)
    this was a lot more annoying the lastfm api tbh (finding the mbids
    mainly (too many physical releases with bad art))
    
    we should probably add mbids to the backend at some point

--- static/js/music_api.js
+++ static/js/music_api.js
@@ -1,63 +1,140 @@
-const API_SECRET = "8c96e05d82dcc8bd73ad54b96dd26c25";
+const MB = "https://musicbrainz.org/ws/2/";
+const CAA = "https://coverartarchive.org/release/";
+const HEADERS = { headers: { 'User-Agent': 'wrct_reviews (eng@wrct.org)'} };
 const parentEl = document.querySelector('#api-info');
 
-let xhr = new XMLHttpRequest();
-xhr.timeout = 5 * 1000; // 5 seconds
-xhr.responseType = 'json';
-
-function getUrl(method, params){
-  var root = "https://ws.audioscrobbler.com/2.0/";
+function getMBUrl(method, mbid, params){
   var allParams = {
-    "method": method,
-    "api_key": API_SECRET,
     ...params,
-    "format": "json",
+    "fmt": "json",
   }
-  return root +  "?" + new URLSearchParams(allParams).toString();
+  const mbidEncoded = mbid ? encodeURIComponent(mbid) : "";
+  return MB + method + "/" + mbidEncoded + "?" + new URLSearchParams(allParams).toString();
 }
 
-function getAlbum(response){
-  var a = response.album;
+async function getAlbumMBID(albumName, artistName) {
+  const albumNameEncoded = encodeURIComponent(albumName);
+  const artistNameEncoded = encodeURIComponent(artistName);
+  const searchUrl =
+    getMBUrl(
+      'release',
+      null,
+      { query: `release:${albumNameEncoded} AND artist:${artistNameEncoded}` }
+    );
+
+  const searchResponse = await fetch(searchUrl, HEADERS);
+
+  const searchData = await searchResponse.json();
+
+  if (!searchData.releases || searchData.releases.length === 0) {
+    return { error: 'Album not found' };
+  }
+
+  const releases = searchData.releases;
+  let mbid = null;
+
+  const digitalRelease = releases.find(release =>
+    release.media && release.media.some(media =>
+      media.format && media.format.toLowerCase().includes('digital')
+    )
+  );
+
+  const cdRelease = digitalRelease ? null :
+    releases.find(release =>
+      release.media && release.media.some(media =>
+        media.format && media.format.toLowerCase() === 'cd'
+      )
+    );
+
+  // Digital over cd over other (musicbrainz has a lot of poor art because of alternative releases)
+  mbid = digitalRelease ? digitalRelease.id : (cdRelease ? cdRelease.id : releases[0].id);
+  return mbid;
+}
+
+// #TODO: this doesnt filter good enough (there are too many results)
+async function getArtistMBID(albumName, artistName) {
+  const albumNameEncoded = encodeURIComponent(albumName);
+  const artistNameEncoded = encodeURIComponent(artistName);
+  const searchUrl =
+    getMBUrl(
+      'artist',
+      null,
+      {
+        query: `release:${albumNameEncoded} AND artist:${artistNameEncoded}`,
+        limit: 1,
+      }
+    );
+  const searchResponse = await fetch(searchUrl, HEADERS);
+  const searchData = await searchResponse.json();
+  if (!searchData.artists || searchData.artists.length === 0) {
+    return { error: 'Artist not found' };
+  }
+  return searchData.artists[0].id;
+}
+
+async function getAlbum(mbid) {
+  const releaseUrl = getMBUrl('release', mbid, { inc: 'recordings+artist-credits', limit: 1 });
+
+  const releaseResponse = await fetch(releaseUrl, HEADERS);
+
+  const releaseData = await releaseResponse.json();
+
+  const coverUrl = `${CAA}${mbid}/front-500`;
+
+  const tracks = releaseData.media[0].tracks.map(track => ({
+    position: track.position,
+    title: track.title,
+    duration: track.length ? Math.floor(track.length / 1000) : null
+  }));
+
+  return {
+    tracks: tracks,
+    coverUrl: coverUrl,
+  };
+}
+
+async function getArtist(mbid) {
+  console.log("unimplemented");
+}
+
+// Very east to add more information, musicbrainz has a lot of data
+async function handleAlbum() {
+  // #TODO: start storing the mbids in the backend to simplify this, it will also
+  // make the artist search much easier. Need to add musicbrainz integration to the
+  // add interface
+  const mbid = await getAlbumMBID(album, artist);
+  const albumInfo = await getAlbum(mbid);
 
   var tracksHTML = ""; var imageHTML = "";
 
-  if (a.hasOwnProperty('image')) {
-    var image = a.image[Math.min(a.image.length, 3)]["#text"];
-    if (image)
-      imageHTML = `<img src="${image}"/>`;
+  if (albumInfo.coverUrl) {
+    imageHTML = `<img src="${albumInfo.coverUrl}" alt="Cover Art"/>`;
   }
 
-  if (a.hasOwnProperty('tracks')) {
-    var tracks = a.tracks.track;
+  if (albumInfo.tracks) {
     tracksHTML = `<div class='no-contents'><h2>Tracks</h2><ol>`;
-    tracks.forEach((track) => {
-      tracksHTML += `<li>${track.name}</li>`
+    albumInfo.tracks.forEach(track => {
+      tracksHTML += `<li>${track.title} - ${track.duration ? `${Math.floor(track.duration / 60)}:${String(track.duration % 60).padStart(2, '0')}` : 'N/A'}</li>`;
     });
     tracksHTML += '</ol></div>';
   }
 
   var section = document.createElement("section");
+
   section.innerHTML = `${imageHTML}
   ${tracksHTML}`;
   parentEl.appendChild(section);
 }
 
-function getArtist(response){
+function getApi() {
+  try {
+    if (table == "Album")
+      handleAlbum();
+    else
+      console.error('currently only handle albums');
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
-function getApi() {
-  var url = getUrl(`${table.toLowerCase()}.getinfo`, {"album": album, "artist": artist});
-  xhr.open('GET', url);
-  xhr.send();
-  xhr.onload = function() {
-    if (xhr.status != 200) { // analyze HTTP status of the response
-      console.log('error', xhr.status, xhr.statusText);
-    } else { // show the result
-      if (table === "Album")
-        getAlbum(xhr.response);
-      else if (table === "Artist")
-        getArtist(xhr.response);
-    }
-  };
-}
 getApi();
